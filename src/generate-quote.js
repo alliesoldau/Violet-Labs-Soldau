9deec82c1b1a5a208aede69d7476c4039b187438
Allie Soldau <alliesoldau@gmail.com>
2023-07-18 14:27:18 -0400

    the random code generator mostly works. something went wrong while getting the json data into the sqlite db where only 1/4 of the data go in. gotta fix that

--- src/generate-quote.js
+++ src/generate-quote.js
@@ -1,7 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var node_fetch_1 = require("node-fetch");
 var data = require('./data/office_quotes.json');
+var url = 'http://localhost:3000/quotes';
 var max = Object.keys(data).length - 1;
 var min = 3;
 var randomId = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-console.log(randomId(min, max));
+var randomNumber = randomId(min, 300);
+(0, node_fetch_1["default"])("".concat(url, "/").concat(randomNumber))
+    .then(function (result) { return result.json(); })
+    //the posted contents to the website in json format is displayed as the output on the screen
+    .then(function (quote) { return console.log(quote.quote, "- ".concat(quote.character)); });
--- src/generate-quote.ts
+++ src/generate-quote.ts
@@ -1,9 +1,20 @@
+import fetch from 'node-fetch';
+
 var data = require('./data/office_quotes.json')
+
+const url = 'http://localhost:3000/quotes'
 
 const max = Object.keys(data).length  - 1
 const min = 3
 
+
+
 const randomId = (min, max) =>
     Math.floor(Math.random() * (max - min + 1)) + min
 
-console.log(randomId(min, max))
+let randomNumber = randomId(min, 300)
+
+fetch(`${url}/${randomNumber}`)
+    .then(result => result.json())
+    //the posted contents to the website in json format is displayed as the output on the screen
+    .then(quote=>console.log(quote.quote, `- ${quote.character}`))
