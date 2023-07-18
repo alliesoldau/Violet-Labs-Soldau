33889006ab4d6416d8be73e6a9e5faaf08071775
Allie Soldau <alliesoldau@gmail.com>
2023-07-18 13:47:55 -0400

    i was able to pull in the json data. now to map through it and post into our database

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 "use strict";
-// import * as quotes from '../src/data/office_quotes.json'
 exports.__esModule = true;
-// console.log(quotes)
 var node_fetch_1 = require("node-fetch");
+// import * as quotes from './data/office_quotes.json'
+var data = require('./data/office_quotes.json');
+console.log(data);
+// This is where I fetch from my server
 (0, node_fetch_1["default"])('http://localhost:3000/quotes')
     .then(function (data) {
     return data.json();
 })
-    .then(function (quote) {
-    console.log(quote);
+    .then(function (quotes) {
+    // console.log(quote)
+    return quotes;
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import fetch from 'node-fetch';
 
+// Pull in office quotes json so we can use it to post into our sql database
+var data = require('./data/office_quotes.json')
+
 // This is where I fetch from my server
 
 fetch('http://localhost:3000/quotes')
 .then(data => {
     return data.json()
 })
-.then(quote => {
-    console.log(quote)
+.then(quotes => {
+    // console.log(quote)
+    return quotes
 })
 
-// import * as quotes from '../src/data/office_quotes.json'
 
-// console.log(quotes)
