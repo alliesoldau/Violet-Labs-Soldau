3fa280c80ec5ed52f6fd84fbeeb57ac081d33e0a
Allie Soldau <alliesoldau@gmail.com>
2023-07-19 09:11:28 -0400

    added simple html front end to display quotes

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable CORS for the entire application so that my html front end can chat with my backend
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
